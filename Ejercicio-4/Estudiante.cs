b2816c3cef72c3c3d0cc2fac11d9db55838c1fb0
william <ofeliavm69@gmail.com>
2023-09-16 20:28:28 -0400

    Ejercicio con listas de Estudiantes

---
+++ Ejercicio-4/Estudiante.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    public class Estudiante : Persona
+    {
+        public string Grado { get; set; }
+        public DateTime FechaIngreso { get; set; }
+    }
+}
---
+++ Ejercicio-4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Estudiante> listEstudiante = new List<Estudiante>();
+            listEstudiante.Add(new Estudiante()
+            {
+                Nombre = "Juan",
+                Apellido = "Perez",
+                Edad = 12,
+                Genero = 'M',
+                Grado = "CUARTO",
+                FechaIngreso = Convert.ToDateTime("02/03/2000")
+
+            });
+            listEstudiante.Add(new Estudiante()
+            {
+                Nombre = "Julio",
+                Apellido = "Apaza",
+                Edad = 12,
+                Genero = 'M',
+                Grado = "CUARTO",
+                FechaIngreso = Convert.ToDateTime("02/03/2000")
+
+            });
+
+            listEstudiante.Add(new Estudiante()
+            {
+                Nombre = "Emmy",
+                Apellido = "Flores",
+                Edad = 16,
+                Genero = 'F',
+                Grado = "SEXTO",
+                FechaIngreso = Convert.ToDateTime("02/03/2000")
+
+            });
+            Console.WriteLine("------------LISTA DE ESTUDIANTES---------------");
+            foreach (var est in listEstudiante)
+            {
+                Console.WriteLine(est.Nombre+ " " + est.Apellido + ", edad: " + est.Edad);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
